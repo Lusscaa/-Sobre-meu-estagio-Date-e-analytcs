4ca2f891a240313d36f37c1122fe2abc177ecd9b
Lusscaa <lucasricardo.silva22@gmail.com>
2023-03-03 11:32:50 -0300

    atulizando

--- biblioteca/ex03.sql
+++ biblioteca/ex03.sql
@@ -1,22 +1,5 @@
 SELECT
     count(*) as quantidade,
-    A.nome,
-    B.estado,
-    B.cidade
-FROM Livro as C
-INNER JOIN editora as A 
-    ON C.editora = B.codeditora
-INNER JOIN endereco as B
-    ON B.endereco = B.codendereco
-GROUP BY A.nome
-ORDER BY quantidade desc
-LIMIT 5
-
-
-
-
-SELECT
-    count(*) as quantidade,
     ed.nome,
     en.estado,
     en.cidade

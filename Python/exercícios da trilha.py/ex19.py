aee78366be932eeb8787baea236d4078f651c535
Lusscaa <lucasricardo.silva22@gmail.com>
2023-03-20 12:09:10 -0300

    Agora sim, bonitim

--- Python/exercícios da trilha.py/ex19.py
+++ Python/exercícios da trilha.py/ex19.py
@@ -1,20 +1,12 @@
-import random 
-
+import random
 random_list = random.sample(range(500), 50)
-
 valor_minimo = min(random_list)
-
 valor_maximo = max(random_list)
-
 media = sum(random_list) / len(random_list)
-
 sorted_list = sorted(random_list)
 tamanho = len(sorted_list)
 if tamanho % 2 == 0:
     mediana = (sorted_list[int(tamanho/2)-1] + sorted_list[int(tamanho/2)]) / 2
 else:
     mediana = sorted_list[int(tamanho/2)]
-
-
-
-print(f"Media: {valor_minimo}, Mediana: {valor_maximo}, Mínimo: {media}, Máximo: {mediana}")
+print(f"Media: {media}, Mediana: {mediana}, Mínimo: {valor_minimo}, Máximo: {valor_maximo}")

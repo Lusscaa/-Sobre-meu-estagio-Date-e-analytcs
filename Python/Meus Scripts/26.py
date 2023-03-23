f52eddfca043cb5fc1cd7229731f5cb905618416
Lusscaa <lucasricardo.silva22@gmail.com>
2023-03-23 09:50:06 -0300

    .

---
+++ Python/Meus Scripts/26.py
@@ -0,0 +1,11 @@
+cidade1 = 'SãoPaulo'
+cidade2 = 'Sul'
+cidade3 = 'Bahia'
+
+cidades = ['RJ', 'SP', 'BH', 'TOP']
+cidades[0] = 'VOS'
+cidades.append('X')
+cidades.remove('SP')
+cidades.insert(0, 'K')
+print(cidades)
+
---
+++ Python/exercícios-da-trilha.py/ETL/info01.py
@@ -0,0 +1,38 @@
+open('Date-e-analytcs/Python/exercícios da trilha.py/ETL/',"r")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# R - Leitura
+# A - Incrimentar
+# W - Escrita
+# X  - Cria
+# R+ Leitura + escrita
